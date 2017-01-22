8655170d1deae9f875a9a458d7e19f041caa63e6
toran billups <toranb@gmail.com>
2017-01-21 22:41:26 -0600

    hacking reselect shim index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+const mergeTrees = require('broccoli-merge-trees')
+const path = require('path')
+
 module.exports = {
-  name: 'ember-reselect-shim'
-};
+  name: 'ember-reselect-shim',
+  treeForAddon (tree) {
+    const reselectPath = path.dirname(require.resolve('reselect/src/index.js'))
+    let reselectTree = this.treeGenerator(reselectPath)
+
+    if (!tree) {
+      return this._super.treeForAddon.call(this, reselectTree)
+    }
+
+    const trees = mergeTrees([reselectTree, tree], {
+      overwrite: true
+    })
+
+    return this._super.treeForAddon.call(this, trees)
+  }
+}
